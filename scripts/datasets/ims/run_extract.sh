eae914d7186cfb75a43527cc24a3441c3e31ffab
leahandofir <leahandofir@gmail.com>
2023-06-11 16:50:15 +0300

    fixed SBATCH file

--- scripts/datasets/ims/run_extract.sh
+++ scripts/datasets/ims/run_extract.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 #SBATCH --job-name=cloud_nowcasting_data_fetch
 #SBATCH --error=%x-%j.out
 #SBATCH --output=%x-%j.out
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
-#SBATCH --time=10:01:00
+#SBATCH --time=48:00:00
 
 module load anaconda3
+source activate leahandofir
 
 python extract_ims_data.py
+
+tail -f
