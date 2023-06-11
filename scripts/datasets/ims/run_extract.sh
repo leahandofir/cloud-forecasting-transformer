ad3bdcb06966cabf4add9038e2bdd9136b3168b2
leahandofir <leahandofir@gmail.com>
2023-06-11 20:37:27 +0300

    prepare big extraction of IR

--- scripts/datasets/ims/run_extract.sh
+++ scripts/datasets/ims/run_extract.sh
@@ -4,8 +4,8 @@
 #SBATCH --error=%x-%j.out
 #SBATCH --output=%x-%j.out
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
-#SBATCH --time=48:00:00
+#SBATCH --mem=16G
+#SBATCH --time=200:00:00
 
 module load anaconda3
 
