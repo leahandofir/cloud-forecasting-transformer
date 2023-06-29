a088486771be2d24719a7e63adde196742cf7e08
leahandofir <leahandofir@gmail.com>
2023-06-29 17:02:37 +0300

    histogram - there is still a bug

--- src/earthformer/visualization/ims/ims_histogram.py
+++ src/earthformer/visualization/ims/ims_histogram.py
@@ -1,35 +1,33 @@
-from earthformer.datasets.ims.ims_dataset import IMSDataset
 import matplotlib.pyplot as plt
-import cv2
+import cv2, os, torch
 import numpy as np
 from omegaconf import OmegaConf
+
 from earthformer.utils.ims.load import load_dataset_params
+from earthformer.datasets.ims.ims_dataset import IMSDataset
+
 
 class IMSHistogram:
     def __init__(self, cfg_file_path):
-        cfg = OmegaConf.load(open(cfg_file_path, "r"))
-        self.dataset = IMSDataset(**load_dataset_params(cfg))
+        self.cfg = OmegaConf.load(open(cfg_file_path, "r"))
+        self.dataset = IMSDataset(**load_dataset_params(self.cfg))
 
-    def plot_hist(self, sample_count=5):
+    def plot_hist(self, sample_count=5, output_path="./", idx_sample=None):
         # https://docs.opencv.org/3.1.0/d1/db7/tutorial_py_histogram_begins.html
+        if idx_sample is None:
+            sample_indices = np.random.choice(range(len(self.dataset)), size=sample_count, replace=False)
+            # reshape to (#samples * T)xHxWxC
+            seq_samples = torch.flatten(torch.stack([self.dataset[i][1] for i in sample_indices]), start_dim=0, end_dim=1)
+        else:
+            seq_samples = self.dataset[idx_sample][1]
 
-        sample_indices = np.random.choice(range(len(self.dataset)), size=sample_count, replace=False)
-        seq_samples = np.stack([self.dataset[i][1] for i in sample_indices])
+        seq_samples = [img for img in seq_samples.numpy()]
         
-        # plot histogram, data is of shape NTHWC
+        # plot histogram, data is of shape (#samples * T)xHxWxC
         hist = cv2.calcHist(images=seq_samples,
-                            channels=[4],
+                            channels=[0],
                             mask=None,
                             histSize=[256],
                             ranges=[0,256])
         plt.plot(hist)
-        plt.savefig('hist_vis.png')
-
-
-# ------------------------------------------------------------------------------
-
-
-ims_hist_obj = IMSHistogram(cfg_file_path="src\\earthformer\\visualization\\ims\\cfg_ims_dataset.yaml")
-
-ims_hist_obj.plot_hist()
-
+        plt.savefig(os.path.join(output_path, f"hist_samples={sample_count}_{self.cfg.img_type}.png"))
