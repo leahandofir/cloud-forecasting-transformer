fed330191349f6850932f0def97dfac0deec0f5e
leahandofir <leahandofir@gmail.com>
2023-06-29 15:02:39 +0300

    working on ims_histogram.py

--- src/earthformer/visualization/ims/ims_histogram.py
+++ src/earthformer/visualization/ims/ims_histogram.py
@@ -1,6 +1,7 @@
 from earthformer.datasets.ims.ims_dataset import IMSDataset
 import matplotlib.pyplot as plt
 import cv2
+import numpy as np
 from omegaconf import OmegaConf
 from earthformer.utils.ims.load import load_dataset_params
 
@@ -9,9 +10,11 @@ class IMSHistogram:
         cfg = OmegaConf.load(open(cfg_file_path, "r"))
         self.dataset = IMSDataset(**load_dataset_params(cfg))
 
-    def plot_hist(self, idx_sample):
+    def plot_hist(self, sample_count=30):
         # https://docs.opencv.org/3.1.0/d1/db7/tutorial_py_histogram_begins.html
-        start_time, seq = self.dataset[idx_sample]
+
+        sample_indices = np.random.choice(range(len(self.dataset)), size=sample_count, replace=False)
+        start_time_sample, seq_sample = self.dataset[sample_indices]
         # plot histogram, data is of shape NTHWC
         hist = cv2.calcHist(images=seq.numpy(),
                             channels=[4],
@@ -20,3 +23,4 @@ class IMSHistogram:
                             ranges=[0,256])
         plt.plot(hist)
         plt.savefig('hist_ir.png')
+
